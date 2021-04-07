ef6974b67c02b6a772895b98b287966f9820f485
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-04-07 23:21:35 +0200

    testDockerTravis

--- travisTest/scriptJava.sh
+++ travisTest/scriptJava.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 
+cd ..
 
 javac travisTest/hello1.java
 javac travisTest/hello2.java
 
 java travisTest/hello1
 java travisTest/hello2
+
+docker image build -t testjava1:1 travisTest
+docker container run -ti testjava1:1
