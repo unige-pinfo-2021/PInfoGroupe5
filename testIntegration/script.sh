35265df3902ecb00252ba8c6687df471e10ccfb4
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-04-20 10:58:31 +0200

    testIntegration

--- testIntegration/script.sh
+++ testIntegration/script.sh
@@ -6,7 +6,7 @@ mvn package
 docker image build -t testintegration:latest .
 cd ..
 
-sudo snap install microk8s --classic
+sudo snap install microk8s --classic --channel=1.21
 
 sudo microk8s start
 sudo microk8s enable dns
