b5627cddb02bad8a10a1b9d0f25d7f5a244cb20e
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-04-23 23:25:33 +0200

    testIntegration

--- travisScripts/buildAfterSuccess.sh
+++ travisScripts/buildAfterSuccess.sh
@@ -15,7 +15,6 @@ sudo apt-get install sshpass
 echo ${privateKey} > key.txt
 chmod 400 key.txt
 
-
-sshpass -p "" ssh -o "StrictHostKeyChecking no" sudo ./serveurConfig/reset.sh
+sshpass -p "" ssh -o "StrictHostKeyChecking no" -i key.txt ${server} sudo ./serveurConfig/reset.sh
 
 echo success
