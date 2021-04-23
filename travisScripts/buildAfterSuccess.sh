3bc7068958b954e7410a3114659cf154d4662c2a
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-04-23 10:35:46 +0200

    testIntegration

--- travisScripts/buildAfterSuccess.sh
+++ travisScripts/buildAfterSuccess.sh
@@ -8,5 +8,8 @@ docker tag api/film khptif/film:latest
 docker push khptif/user:latest
 docker push khptif/film:latest
 
+sudo apt-get install openssh-server
 sudo apt-get install sshpass
 sshpass -p ${serverPassword} ssh ${server} sudo ./serveurConfig/reset.sh
+
+echo success
