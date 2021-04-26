7fec1cbfc86177a95831674c64eddf0796a0dc59
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-04-26 22:43:05 +0200

    merge

---
+++ algorythmic_selector/src/main/java/selector/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package selector.rest; //.films.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
---
+++ algorythmic_selector/src/main/java/selector/rest/SelectorRestService.java
@@ -0,0 +1,25 @@
+package selector.rest;
+
+
+import java.util.*;
+
+import selector.model.*;
+
+import java.io.IOException;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
+
+@Path("/selector")
+public class SelectorRestService {
+
+    
+    
+
+}//end class
