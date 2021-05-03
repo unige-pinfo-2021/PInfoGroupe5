60f45406d8a0afe4427ce51ed27e29ca7d496f28
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-05-03 19:07:16 +0200

    groupMock

--- group/src/main/java/api/rest/RestServiceGroup.java
+++ group/src/main/java/api/rest/RestServiceGroup.java
@@ -15,6 +15,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.PathParam;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 @Path("/group")
