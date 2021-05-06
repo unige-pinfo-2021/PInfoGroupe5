d858110a4ec51a4345471af03ef224c6b466d661
Fatih.Sarikaya <git@gitlab.unige.ch:Fatih.Sarikaya/semantique_2021.git>
2021-05-06 09:55:54 +0200

    selector

---
+++ algorythmic_selector/src/main/java/selector/model/selector.java
@@ -0,0 +1,57 @@
+package selector.model;
+
+import java.util.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import org.json.*;
+import java.net.*;
+import java.io.*;
+
+public class selector {
+
+    public selector()throws IOException, InterruptedException
+    {
+        FileWriter myWriter = new FileWriter("/etc/hosts");
+		myWriter.write("\n 129.194.10.130 tindfilm");
+		myWriter.close();
+    }
+
+    public String algorithme(String critereJson)throws IOException, InterruptedException
+    {
+        JSONObject critere = new JSONObject(critereJson);
+
+        String catalogue = get("tindfilm/film");
+        return catalogue;
+
+    }
+
+    private String get(String adresse)throws IOException, InterruptedException 
+    {
+    	URL obj = new URL(adresse);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			return response.toString();
+		} else {
+			return "GET request not worked";
+		}
+       
+    }
+}
--- algorythmic_selector/src/main/java/selector/rest/SelectorRestService.java
+++ algorythmic_selector/src/main/java/selector/rest/SelectorRestService.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 
@@ -17,9 +18,28 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.PathParam;
 
 @Path("/selector")
-public class SelectorRestService {
+public class SelectorRestService
+{
+   private selector selecteur;
 
-    
-    
+   public SelectorRestService()throws IOException, InterruptedException
+   {
+       this.selecteur = new selector();
+   }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String Default()
+    {
+        return "You reached selector";
+    }
+
+    @POST 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/catalogue")
+    public String getCatalogue(){
+	    return "catalogue";
+    }
+    
 }//end class
