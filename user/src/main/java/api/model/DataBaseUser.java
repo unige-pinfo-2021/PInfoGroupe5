79decb17d2c35d62316b440a0b43359549ec3d79
kkf17 <favre.kathleen@orange.fr>
2021-05-27 14:39:32 +0200

    userupdtK

--- user/src/main/java/api/model/DataBaseUser.java
+++ user/src/main/java/api/model/DataBaseUser.java
@@ -22,11 +22,11 @@ public class DataBaseUser{
 	private String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //"org.postgresql.Driver"
 
 	public DataBaseUser(String path){
-		//Properties props =  new DataBaseUserProperties().readProperties(path);
+		/*Properties props =  new DataBaseUserProperties().readProperties(path);
 
-		//this.url=props.getProperty("db.url");
-		//this.username=props.getProperty("db.user");
-		//this.password=props.getProperty("db.passwd");
+		this.url=props.getProperty("db.url");
+		this.username=props.getProperty("db.user");
+		this.password=props.getProperty("db.passwd");*/
 
 		this.url="jdbc:mysql://129.194.10.130:3306/tinderfilmBD";
 		this.username = "groupe5";
@@ -89,13 +89,14 @@ public class DataBaseUser{
 
 	public ArrayList<Map<String,String>> SELECT_User(String username){
 		//String query ="SELECT *  FROM Users";
-		String query ="SELECT * FROM user WHERE name="+"'"+username+"';";
-		return get_User(query);
+		//String query ="SELECT * FROM user WHERE name="+"'"+username+"';";
+		String query ="SELECT * FROM user WHERE name=?";
+		return get_User(query, username);
 	}//end INSERT_User
 
 	public ArrayList<Map<String,String>> SELECT_AllUser(){
 		String query ="SELECT * FROM user;";
-		return get_User(query);
+		return get_User(query, null);
 	}//end INSERT_User
 
 	//public void UPDATE_User(String username){}
@@ -132,13 +133,18 @@ public class DataBaseUser{
 
     }//end set_User
 
-	public ArrayList<Map<String,String>> get_User(String query){
+	public ArrayList<Map<String,String>> get_User(String query, String username){
 		ArrayList<Map<String,String>> params = new ArrayList();
 		Connection conn = null;
 		try{
 			Class.forName(JDBC_DRIVER); 
 			conn = DriverManager.getConnection(this.url, this.username, this.password);
 			PreparedStatement pst = conn.prepareStatement(query);
+
+			if(username != null){
+				pst.setString(1, username);
+			}
+
 			ResultSet rs = pst.executeQuery();
 
 			while (rs.next()) {
