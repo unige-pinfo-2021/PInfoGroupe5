b333844440dce0f6bdeeb6b5df610c9bfee3c7c2
frederic necker <frederic.necker@etu.unige.ch>
2021-05-30 15:12:22 +0200

    a few code smells

--- film/src/main/java/api/rest/RestServiceFilms.java
+++ film/src/main/java/api/rest/RestServiceFilms.java
@@ -40,15 +40,15 @@ public class RestServiceFilms {
     		@PathParam("page") int page,@PathParam("primary_release_date.gte") String primary_release_date_gte,@PathParam("primary_release_date.lte") String primary_release_date_lte,@PathParam("with_people") String with_people,
     		@PathParam("with_genres") String with_genres,@PathParam("with_keywords") String with_keywords) throws IOException, InterruptedException { 
     	String requestJson = "{\n"
-				+ "	\"include_adult\": false,\n"
-				+ "	\"vote_average.gte\": "+vote_average_gte+",\n"
-				+ "	\"vote_average.lte\": "+vote_average_lte+",\n"
-				+ "	\"page\": "+page+",\n"
-				+ "	\"primary_release_date.gte\": "+primary_release_date_gte+",\n"
-				+ "	\"primary_release_date.lte\": "+primary_release_date_lte+",\n"
-				+ "	\"with_people\": "+with_people+",\n"
-				+ "	\"with_genres\": "+with_genres+",\n"
-				+ "	\"with_keywords\": "+with_keywords+"\n"
+				+ "\t\"include_adult\": false,\n"
+				+ "\t\"vote_average.gte\": "+vote_average_gte+",\n"
+				+ "\t\"vote_average.lte\": "+vote_average_lte+",\n"
+				+ "\t\"page\": "+page+",\n"
+				+ "\t\"primary_release_date.gte\": "+primary_release_date_gte+",\n"
+				+ "\t\"primary_release_date.lte\": "+primary_release_date_lte+",\n"
+				+ "\t\"with_people\": "+with_people+",\n"
+				+ "\t\"with_genres\": "+with_genres+",\n"
+				+ "\t\"with_keywords\": "+with_keywords+"\n"
 				+ "}";
     	return filmService.executeRequest_asList(requestJson);
     }
