4caf1c1e3f99de550f7745ccb9b782d592ef55d7
Bernard Klacar <bernard.klacar@etu.unige.ch>
2021-06-09 02:07:01 +0200

    front v11

--- web-ui/src/app/recommendation/recommendation.component.ts
+++ web-ui/src/app/recommendation/recommendation.component.ts
@@ -57,22 +57,20 @@ export class RecommendationComponent implements OnInit {
 
         //check if user in DB
         this.userService.updateUserDB(userName,userEmail);
+
         //get recommended movies
-        this.groupService.createCatalogue(groupName,userName).
-        subscribe(
-          data =>{
-            this.groupService.getCatalogue(groupName,userName)
-              .subscribe(
-                data =>{
-                  console.log(data);
-                  this.films = data;
-                }
-            );
-          }
-      );
-
-
-
+        this.groupService.createCatalogue(groupName,userName)
+          .subscribe(
+            data =>{
+              this.groupService.getCatalogue(groupName,userName)
+                .subscribe(
+                  data =>{
+                    console.log(data);
+                    this.films = data;
+                  }
+                );
+            }
+          );
       }
     );
 
